d075c6c5d4de304a96f31513026cfaeffaecf2a2
Fatma Bostan <fatma_bostan_@outlook.com>
2023-07-17 23:55:06 +0300

    change

--- ContactDesign/src/components/Card.jsx
+++ ContactDesign/src/components/Card.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Avatar from "./Avatar";
+import Detail from "./Detail";
 
-const Card = (props) =>
-{
-    return(<div className="card">
-        <div className="top">  
-        <p className="name">{props.name}</p>
-        <Avatar img={props.img}/>
+const Card = (props) => { return (
+        <div className="card">
+            <div className="top">
+            <p className="name">{props.name}</p>
+            <Avatar img={props.img} />
+            </div>
+            <div className="bottom">
+            <Detail detailInfo={props.tel}/>
+            <Detail detailInfo={props.mail}/>
+            </div>
         </div>
-
-        <div className="bottom">
-        <p className="info">{props.mail}<br/>{props.tel}</p></div></div>
-    )
+         )
 }
+
 export default Card;

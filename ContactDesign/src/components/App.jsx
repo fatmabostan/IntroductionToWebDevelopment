de188d03e5a3c87cf9a7ac9f10d58b1b4dbaa2c3
Fatma Bostan <fatma_bostan_@outlook.com>
2023-07-18 10:05:56 +0300

    new designed

--- ContactDesign/src/components/App.jsx
+++ ContactDesign/src/components/App.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Card from "./Card";
 import Contact from "./Contact";
 
+const ContactUse = (props) => {
+    return <Card key={props.id} name={props.name} mail={props.mail} tel={props.tel} img={props.img} />
+}
+
 const App = () => {
     // useState lr slınsbilir bu dsnstçı idmi vr linkleri
 return(<div>
     <h1 className="heading">My Contacts</h1>
-   { Contact.map((value) => <div key={value.id}><Card name={value.name} img={value.img} mail={value.mail} tel={value.tel}/></div>)}
+   {Contact.map(ContactUse)}
 </div>)
 }
 

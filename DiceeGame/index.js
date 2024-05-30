e37603478763fb087a8b9d57c0e9e410fe8ce81d
Fatma Bostan <fatma_bostan_@outlook.com>
2024-05-30 21:05:25 +0300

    ..

---
+++ DiceeGame/index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = (Math.floor(Math.random()*6)+1);
+var randomNumber2 = (Math.floor(Math.random()*6)+1);
+
+
+const imgFor1 = document.getElementsByClassName("img1")[0];
+imgFor1.setAttribute("src", "/images/dice"+randomNumber1+".png");
+
+const imgFor2 = document.getElementsByClassName("img2")[0];
+imgFor2.setAttribute("src", "/images/dice"+randomNumber2+".png");
+
+const heading = document.querySelector("h1");
+
+if(randomNumber1 > randomNumber2)
+    {
+        heading.innerHTML="You win!"
+    }
+else if (randomNumber1 === randomNumber2) {
+    heading.innerHTML="The match ended in a draw!"
+
+} else {
+    heading.innerHTML="You lose!"
+
+}

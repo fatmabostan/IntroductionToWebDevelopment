d9c41a414f738322d8b535a4ed80b9e6c2a75607
fatmabostan <fatma_bostan_@outlook.com>
2024-12-02 04:08:10 +0300

    initial commit

---
+++ 293-keeper-app-part-1-challenge/src/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note(){
+    return(
+    <div className='note'>
+        <h1>This is title for Keeper App</h1>
+        <p>This is content for Keeper App</p>
+    </div>
+    )
+}
+
+export default Note;
